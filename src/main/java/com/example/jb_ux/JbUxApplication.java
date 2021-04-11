672f24c0bca0960f4b05fcf2e965989d756ca7cd
Jack <rxstr1ker007@gmail.com>
2021-04-11 19:20:15 +0300

    Initial commit

---
+++ src/main/java/com/example/jb_ux/JbUxApplication.java
@@ -0,0 +1,13 @@
+package com.example.jb_ux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbUxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbUxApplication.class, args);
+    }
+
+}
