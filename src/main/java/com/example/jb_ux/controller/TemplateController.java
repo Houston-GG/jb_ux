7a0219135f8967cf2d1e3ec323e82c59fa18adad
Jack <rxstr1ker007@gmail.com>
2021-04-11 20:35:13 +0300

    [FEAT] Added exception resolving to saveNewTemplate method.

--- src/main/java/com/example/jb_ux/controller/TemplateController.java
+++ src/main/java/com/example/jb_ux/controller/TemplateController.java
@@ -4,7 +4,11 @@ import com.example.jb_ux.dto.template.NewTemplate;
 import com.example.jb_ux.dto.template.TemplateDto;
 import com.example.jb_ux.service.TemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -27,7 +31,13 @@ public class TemplateController {
 
     @PostMapping("/template")
     public void saveNewTemplate(@RequestBody @Valid NewTemplate newTemplate) {
-        templateService.saveNewTemplate(newTemplate);
+        try {
+            templateService.saveNewTemplate(newTemplate);
+        } catch (InvalidDataAccessApiUsageException exception) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Selected unknown dispatch method.");
+        } catch (DataIntegrityViolationException exception) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Template with ID [" + newTemplate.getTemplateId() + "] already exists.");
+        }
 
     }
 }
