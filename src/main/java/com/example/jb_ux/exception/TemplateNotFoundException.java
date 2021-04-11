092cf039520f952cd875a841246fa04b6128663e
Jack <rxstr1ker007@gmail.com>
2021-04-11 20:37:26 +0300

    [FEAT] Added TemplateNotFoundException.

---
+++ src/main/java/com/example/jb_ux/exception/TemplateNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.jb_ux.exception;
+
+public class TemplateNotFoundException extends RuntimeException{
+    public TemplateNotFoundException(String message) {
+        super(message);
+    }
+}
