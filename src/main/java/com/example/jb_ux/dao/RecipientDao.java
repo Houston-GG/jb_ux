1df898158673a5dba33eabf5c175296ce3809f4a
Jack <rxstr1ker007@gmail.com>
2021-04-11 20:22:25 +0300

    [FEAT] Added doa (RecipientDao, TemplateDao).

---
+++ src/main/java/com/example/jb_ux/dao/RecipientDao.java
@@ -0,0 +1,7 @@
+package com.example.jb_ux.dao;
+
+import com.example.jb_ux.model.Recipient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipientDao extends CrudRepository<Recipient, Long> {
+}
